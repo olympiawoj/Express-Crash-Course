5a2d648940d16f1380c59c3132ad0b9877b3eb2f
olympia <owojcik7@gmail.com>
2019-12-03 12:03:54 -0500

    Create basic express server

---
+++ index.js
@@ -0,0 +1,14 @@
+//Bring in express
+
+const express = require('express')
+
+//initialize variable app w/ express
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send('<h1>Hello World</>')
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`))
