03053f9bdb77b20d561fcc48f0bd02bf69f30397
olympia <owojcik7@gmail.com>
2019-12-04 16:30:05 -0500

    Install nodemon

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const express = require('express')
 const app = express()
 
 app.get("/", (req, res) => {
-    res.send('<h1>ey hellllooo sup World!</>')
+    res.send('<h1>Hey World!</> ')
 })
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`))
