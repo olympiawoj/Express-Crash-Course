752f9409893cd48b3eaf1e0dc69342a5585eb734
olympia <owojcik7@gmail.com>
2019-12-04 20:21:26 -0500

    Add logger middleware

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 //Bring in express
 const express = require('express')
 const path = require('path')
+const moment = require('moment')
 const members = require('./Members')
 
 //initialize variable app w/ express
 const app = express()
 
+//middleware
+const logger = (req, res, next) => {
+    console.log('Middleware dispatching')
+    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}:${moment().format()}`)
+    next()
+}
+
+//Init middleware
+app.use(logger)
 
 //This route Gets all  members
 app.get('/api/members', (req, res) => {
