1a4b1cbe2351663fa83339ad941ad3ea68aded77
olympia <owojcik7@gmail.com>
2019-12-04 20:10:12 -0500

    Create route that returns JSON

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 //Bring in express
 const express = require('express')
 const path = require('path')
+const members = require('./Members')
 
 //initialize variable app w/ express
 const app = express()
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+
+//This route Gets all  members
+app.get('/api/members', (req, res) => {
+    //return some JSON
+    res.json(members)
 })
+
+//Set a static folder- use is method we use when we wanna include middleware
+//Point to folder we want to set as static folder
+app.use(express.static(path.join(__dirname, "public")))
 
 const PORT = process.env.PORT || 3000
 
